a58393083c3a7467ffb0fd85fbaed536bcdf497e
FernandoBalbin <72724516@continental.edu.pe>
2021-09-21 11:38:30 -0500

    Linea del proyecto

--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -1,6 +1,9 @@
 import unittest
-
+from src.Conjunto import Conjunto
 
 class TestConjunto(unittest.TestCase):
+    def test_conjunto_vacio_retornaNone(self):
+        conjunto = Conjunto()
+        self.assertIsNone(conjunto.promedio())
 
     pass
